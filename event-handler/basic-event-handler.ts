729e8dfa83ab6e891390aba43d2c7be45fc1a07c
Simon Davis <simonfreelance1@gmail.com>
2021-06-25 14:08:36 -0400

    started on basic-event-handler challenge

--- event-handler/basic-event-handler.ts
+++ event-handler/basic-event-handler.ts
@@ -1,17 +1,36 @@
-class EventProcessor {
-  handleEvent(eventName: ..., data: ...): void {
+type Filter<T> = (data: T) => boolean;
+type Map<T> = (data: T) => Partial<T>;
+type Processors<T> = {[Property in keyof T]: (Map | Filter)}
+class EventProcessor<EventMap> {
+  private processedEvents: EventMap[] = [];
+  public processors: {
+    [Property in keyof EventMap]: (Filter<EventMap[Property]|Map<EventMap[Property])[];
+  } = {};
+  handleEvent<Key extends keyof EventMap>(eventName: Key, data: EventMap[Key]): void {
+
   }
 
-  addFilter(
-    eventName: ...,
-    filter: (data: ...) => boolean
+  addFilter<Key extends keyof EventMap>(
+    eventName: Key,
+    filter: Filter<EventMap[Key]>
   ): void {
+    if(this.processors[eventName]) {
+      this.processors[eventName].push(filter);
+    } else {
+      this.processors[eventName] = [filter]
+    }
   }
 
-  addMap(eventName: ..., map: (data: ...) => ...): void {
+  addMap<Key extends keyof EventMap>(eventName: Key, map: Map<EventMap[Key]>): void {
+     if(this.processors[eventName]) {
+      this.processors[eventName].push(map);
+    } else {
+      this.processors[eventName] = [map]
+    }
   }
 
   getProcessedEvents() {
+    console.table(JSON.stringify(this.processedEvents))
   }
 }
 
@@ -20,30 +39,32 @@ interface EventMap {
   logout: { user?: string };
 }
 
-class UserEventProcessor extends EventProcessor<EventMap> {}
+const ep = new EventProcessor<EventMap>();
+ep.processors
+// class UserEventProcessor extends EventProcessor<EventMap> {}
 
-const uep = new UserEventProcessor();
+// const uep = new UserEventProcessor();
 
-uep.addFilter("login", ({ user }) => Boolean(user));
+// uep.addFilter("login", ({ user }) => Boolean(user));
 
-uep.addMap("login", (data) => ({
-  ...data,
-  hasSession: Boolean(data.user && data.name),
-}));
+// uep.addMap("login", (data) => ({
+//   ...data,
+//   hasSession: Boolean(data.user && data.name),
+// }));
 
-uep.handleEvent("login", {
-  user: null,
-  name: "jack",
-});
-uep.handleEvent("login", {
-  user: "tom",
-  name: "tomas",
-});
-uep.handleEvent("logout", {
-  user: "tom",
-});
+// uep.handleEvent("login", {
+//   user: null,
+//   name: "jack",
+// });
+// uep.handleEvent("login", {
+//   user: "tom",
+//   name: "tomas",
+// });
+// uep.handleEvent("logout", {
+//   user: "tom",
+// });
 
-console.log(uep.getProcessedEvents());
+// console.log(uep.getProcessedEvents());
 
 /*
 Result:

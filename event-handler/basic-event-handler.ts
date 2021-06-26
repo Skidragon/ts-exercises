a4db359fd2ca748adf3479c94c621b28692cc184
Simon Davis <simonfreelance1@gmail.com>
2021-06-26 07:15:49 -0400

    implemented addFilter and addMap handlers

--- event-handler/basic-event-handler.ts
+++ event-handler/basic-event-handler.ts
@@ -1,12 +1,35 @@
 type Filter<T> = (data: T) => boolean;
 type Map<T> = (data: T) => Partial<T>;
-type Processors<T> = {[Property in keyof T]: (Map | Filter)}
+enum ProcessorTypes {
+  map,
+  filter
+}
+type Processor<Value> = { processor: Filter<Value>|Map<Value>; type: ProcessorTypes; };
 class EventProcessor<EventMap> {
-  private processedEvents: EventMap[] = [];
-  public processors: {
-    [Property in keyof EventMap]: (Filter<EventMap[Property]|Map<EventMap[Property])[];
-  } = {};
+  //TODO: data needs to have the value of EventMap
+  private processedEvents: { eventName: ProcessorTypes, data: any}[] = [];
+  public processors = {} as {
+    [Key in keyof EventMap]: Processor<EventMap[Key]>[];
+  }
   handleEvent<Key extends keyof EventMap>(eventName: Key, data: EventMap[Key]): void {
+    const event = {
+      eventName,
+      data
+    }
+    for(let { processor, type } of this.processors[eventName]) {
+        if(type === ProcessorTypes.filter) {
+          if(processor(data) === true) {
+            continue;
+          }
+        }
+        if(type === ProcessorTypes.map) {
+          //TODO: Map the data
+          processor(data);
+          event.data = {
+            ...event.data,
+          }
+        }
+    }
 
   }
 
@@ -14,57 +37,65 @@ class EventProcessor<EventMap> {
     eventName: Key,
     filter: Filter<EventMap[Key]>
   ): void {
+    const processor = {
+      type: ProcessorTypes.filter,
+        processor: filter
+    }
     if(this.processors[eventName]) {
-      this.processors[eventName].push(filter);
+      this.processors[eventName].push(processor);
     } else {
-      this.processors[eventName] = [filter]
+      this.processors[eventName] = [processor]
     }
   }
 
   addMap<Key extends keyof EventMap>(eventName: Key, map: Map<EventMap[Key]>): void {
+    const processor = {
+      type: ProcessorTypes.map,
+      processor: map
+    }
      if(this.processors[eventName]) {
-      this.processors[eventName].push(map);
+      this.processors[eventName].push(processor);
     } else {
-      this.processors[eventName] = [map]
+      this.processors[eventName] = [processor]
     }
   }
 
   getProcessedEvents() {
-    console.table(JSON.stringify(this.processedEvents))
+    return JSON.stringify(this.processedEvents);
   }
 }
 
 interface EventMap {
-  login: { user?: string; name?: string; hasSession?: boolean };
+  //TODO: Make it work with user?: string
+  login: { user?: string | null; name?: string; hasSession?: boolean };
   logout: { user?: string };
 }
 
 const ep = new EventProcessor<EventMap>();
-ep.processors
-// class UserEventProcessor extends EventProcessor<EventMap> {}
+class UserEventProcessor extends EventProcessor<EventMap> {}
 
-// const uep = new UserEventProcessor();
+const uep = new UserEventProcessor();
 
-// uep.addFilter("login", ({ user }) => Boolean(user));
+uep.addFilter("login", ({ user }) => Boolean(user));
 
-// uep.addMap("login", (data) => ({
-//   ...data,
-//   hasSession: Boolean(data.user && data.name),
-// }));
+uep.addMap("login", (data) => ({
+  ...data,
+  hasSession: Boolean(data.user && data.name),
+}));
 
-// uep.handleEvent("login", {
-//   user: null,
-//   name: "jack",
-// });
-// uep.handleEvent("login", {
-//   user: "tom",
-//   name: "tomas",
-// });
-// uep.handleEvent("logout", {
-//   user: "tom",
-// });
+uep.handleEvent("login", {
+  user: null,
+  name: "jack",
+});
+uep.handleEvent("login", {
+  user: "tom",
+  name: "tomas",
+});
+uep.handleEvent("logout", {
+  user: "tom",
+});
 
-// console.log(uep.getProcessedEvents());
+console.table(uep.getProcessedEvents());
 
 /*
 Result:
